9c1aaf3b89e79a9c0cc61c253d85afe952fd9933
Raul osuna <alC22760487@ite.edu.mx>
2026-03-21 20:00:40 -0700

    Instantiate Gato and Perro classes
    Create instances of Gato and Perro classes and use them.

---
+++ ejercicio5/main.py
@@ -0,0 +1,15 @@
+from Perro import Perro
+from Gato import Gato
+
+# 1. Crear las instancias
+
+pelusa = Gato("Pelusa",4)
+naranjo = Gato("Naranjo",2)
+
+solovino = Perro("Solovino",6)
+boton = Perro("Boton",8)
+
+# 2. Usar las instancias
+pelusa.hablar()
+solovino.hablar()
+solovino.describir()
